18f3a3061f29c71d623556b0690b91d29587c5d6
ZTI <zhaotianff@163.com>
2019-10-31 11:39:56 +0800

    Util.ps1

---
+++ Util.ps1
@@ -0,0 +1,5 @@
+#Change System Font
+#HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts
+#Microsoft JhengHei (TrueType) & Microsoft JhengHei UI (TrueType) msjh.ttc
+#Microsoft JhengHei Bold (TrueType) & Microsoft JhengHei UI Bold (TrueType) msjhbd.ttc
+#Microsoft JhengHei Light (TrueType) & Microsoft JhengHei UI Light (TrueType) msjhl.ttc
