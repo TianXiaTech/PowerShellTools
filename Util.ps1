1d139e97fdb10df68d2799b5e1331139c09b38b6
ZTI <zhaotianff@163.com>
2019-11-07 16:44:25 +0800

    update Util.ps1

--- Util.ps1
+++ Util.ps1
@@ -3,3 +3,10 @@
 #Microsoft JhengHei (TrueType) & Microsoft JhengHei UI (TrueType) msjh.ttc
 #Microsoft JhengHei Bold (TrueType) & Microsoft JhengHei UI Bold (TrueType) msjhbd.ttc
 #Microsoft JhengHei Light (TrueType) & Microsoft JhengHei UI Light (TrueType) msjhl.ttc
+
+#Get hardware info
+#Computer\HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Enum\
+
+#WMI
+#https://docs.microsoft.com/zh-cn/windows/win32/cimwin32prov/computer-system-hardware-classes
+#Get-WmiObject Win32_DiskDrive |Format-List -Property *
